a78cc0a7413cc3afdef38d3a0e6fd3af080346a6
Harshwardhan <harshw.pauranik@gmail.com>
2019-10-22 12:08:53 +0530

    Initializing the Project

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="container">
+        <app-home></app-home>
+    </div>
+</template>
+
+<script>
+import Home from './components/Home.vue';
+export default {
+        components : {
+            appHome : Home
+        }
+    }
+</script>
+
+<style>
+
+</style>
